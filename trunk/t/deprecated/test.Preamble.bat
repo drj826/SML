091be608f5195d11ea97759242a9fd60dabed2e9
drj826@gmail.com <drj826@gmail.com@eb76c438-2348-b3a9-3cae-b09b162631f7>
2014-12-31 14:41:53 +0000

    Deleted deprecated test cases

--- trunk/t/deprecated/test.Preamble.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set begin=%DATE% %TIME%
-echo BEGIN: %begin%
-
-..\..\perl\perl\bin\perl.exe Preamble.t
-
-echo BEGIN: %begin%
-echo END:   %DATE% %TIME%
--- trunk/t/deprecated/test.RenderableAsCSV.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set begin=%DATE% %TIME%
-echo BEGIN: %begin%
-
-..\..\perl\perl\bin\perl.exe RenderableAsCSV.t
-
-echo BEGIN: %begin%
-echo END:   %DATE% %TIME%
