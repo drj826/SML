95c9b8b5eeca490956c7b7ad478b77bd4ea8349b
drj826@gmail.com <drj826@gmail.com@eb76c438-2348-b3a9-3cae-b09b162631f7>
2015-01-12 01:14:06 +0000

    Updated publish_html.pl after moving most functionality into the SML::Formatter.pm class

--- trunk/bin/publish_html.pl
+++ trunk/bin/publish_html.pl
@@ -16,51 +16,20 @@ my $filespec = shift;                   # document file
 my $template_dir = '/home/drj826/semlang/templates/html/default';
 my $output_dir   = "/home/drj826/$id";
 
-my $library  = SML::Library->new(config_filename=>'library.conf');
-my $parser   = $library->get_parser;
-my $fragment = $parser->parse($filespec);
-my $document = $library->get_document($id);
+my $library   = SML::Library->new(config_filename => 'library.conf');
+my $parser    = $library->get_parser;
+my $fragment  = $parser->parse($filespec);
+my $document  = $library->get_document($id);
+my $formatter = $library->get_formatter;
 
 die "DOCUMENT NOT IN LIBRARY: \'$id\'" if not $document;
 
-my $tt_config =
-  {
-   INCLUDE_PATH => "$template_dir",
-   OUTPUT_PATH  => "$output_dir",
-  };
-
-my $tt = Template->new($tt_config) || die "$Template::ERROR\n";
-
-my $vars =
-  {
-   document => $document,
-  };
-
-foreach my $page ('titlepage','toc','glossary','index','references')
-  {
-    my $outfile = "$id.$page.html";
-    $logger->info("formatting $outfile");
-    $tt->process("$page.tt",$vars,$outfile) || die $tt->error(), "\n";
-  }
-
-foreach my $section (@{ $document->get_section_list })
-  {
-    my $num = $section->get_number;
-
-    $num =~ s/\./-/g;
-
-    my $outfile = "$id.$num.html";
-
-    my $vars =
-      {
-       document => $document,
-       section  => $section,
-      };
-
-    $logger->info("formatting $outfile");
-    $tt->process('section_page.tt',$vars,$outfile)
-      || die $tt->error(), "\n";
-  }
+$formatter->publish_html_by_section
+  (
+   $document,
+   $template_dir,
+   $output_dir,
+  );
 
 ######################################################################
 
