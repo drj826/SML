21965cf7e78e84c73e307f940f701c08fe74005e
drj826@gmail.com <drj826@gmail.com@eb76c438-2348-b3a9-3cae-b09b162631f7>
2015-02-01 15:18:22 +0000

    Updated publish_html.pl to fix path bugs and add a 'style' option

--- trunk/bin/publish_html.pl
+++ trunk/bin/publish_html.pl
@@ -12,18 +12,19 @@ my $docid;
 my $doclibdir   = "$Bin/../library";
 my $templatedir = "$Bin/../templates";
 my $configdir   = "$Bin/../conf";
-my $outputdir   = "$Bin/..";
+my $outputdir   = "$Bin/../..";
 my $inclibdir   = "$Bin/../lib";
+my $style       = 'default';
 
 GetOptions
   (
-   "f|filespec=s"    => \$filespec,
-   "d|docid=s"       => \$docid,
-   "l|doclibdir:s"   => \$doclibdir,
-   "t|templatedir:s" => \$templatedir,
-   "c|configdir:s"   => \$configdir,
-   "o|outputdir:s"   => \$outputdir,
-   "i|inclibdir:s"   => \$inclibdir,
+   "f|filespec=s"    => \$filespec,     # fragment file
+   "d|docid=s"       => \$docid,        # document ID
+   "l|doclibdir:s"   => \$doclibdir,    # document library directory
+   "t|templatedir:s" => \$templatedir,  # template directory
+   "c|configdir:s"   => \$configdir,    # configuration directory
+   "o|outputdir:s"   => \$outputdir,    # output directory
+   "s|style:s"       => \$style,        # formatting style
   );
 
 ######################################################################
@@ -38,19 +39,22 @@ use Template;
 
 use SML::Library;
 
-my $library   = SML::Library->new(config_filename=>'library.conf');
-my $parser    = $library->get_parser;
-my $fragment  = $parser->parse($filespec);
-my $document  = $library->get_document($docid);
-my $formatter = $library->get_formatter;
+my $library  = SML::Library->new(config_filename=>'library.conf');
+my $parser   = $library->get_parser;
+my $fragment = $parser->parse($filespec);
+my $document = $library->get_document($docid);
 
 die "DOCUMENT NOT IN LIBRARY: \'$docid\'" if not $document;
+
+my $template_dir = "$templatedir/html/$style";
+my $output_dir   = "$outputdir/$docid";
+my $formatter    = $library->get_formatter;
 
 $formatter->publish_html_by_section
   (
    $document,
-   $templatedir,
-   $outputdir,
+   $template_dir,
+   $output_dir,
   );
 
 ######################################################################
