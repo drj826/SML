f35c0e31294d446be1adeb8b391123496536fec2
drj826 <drj826@gmail.com>
2015-03-09 21:46:10 +0000

    Updated Entity.pm to delete deprecated code

--- lib/SML/Entity.pm
+++ lib/SML/Entity.pm
@@ -40,7 +40,6 @@ sub validate {
   foreach my $block (@{ $self->get_block_list })
     {
       if ( not $block->validate_syntax ) {
-	# $self->set_valid(0);
 	$valid = 0;
       }
     }
@@ -48,12 +47,10 @@ sub validate {
   foreach my $element (@{ $self->get_element_list })
     {
       if ( not $element->validate_syntax ) {
-	# $self->set_valid(0);
 	$valid = 0;
       }
 
       if ( not $element->validate_resource_availability ) {
-	# $self->set_valid(0);
 	$valid = 0;
       }
     }
@@ -61,12 +58,10 @@ sub validate {
   foreach my $division (@{ $self->get_division_list })
     {
       if ( not $division->validate_semantics ) {
-	# $self->set_valid(0);
 	$valid = 0;
       }
 
       if ( not $division->validate_composition ) {
-	# $self->set_valid(0);
 	$valid = 0;
       }
     }
