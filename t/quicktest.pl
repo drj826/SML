3b2f1628a1bcd96c9d77ffea81e1c4e4d019ef19
drj826 <drj826@gmail.com>
2015-03-13 21:00:21 +0000

    Updated unit tests

--- t/quicktest.pl
+++ t/quicktest.pl
@@ -4,45 +4,31 @@
 
 ######################################################################
 
+my $filename = 'td-000066.txt';
+my $docid    = 'td-000066';
+
 use SML::Library;
 
 use Log::Log4perl;
 Log::Log4perl->init("log.test.conf");
 my $logger = Log::Log4perl::get_logger('sml.application');
 
-use Test::Log4perl;
+my $library  = SML::Library->new(config_filename=>'library.conf');
+my $parser   = $library->get_parser;
+my $fragment = $parser->create_fragment($filename);
+my $document = $library->get_document($docid);
 
-my $library = SML::Library->new(config_filename=>'library.conf');
-my $parser  = $library->get_parser;
+my $valid = $document->is_valid;
 
-my $tc =
+if ($valid)
   {
-   name        => 'invalid_semantics_division_1',
-   testfile    => 'td-000063.txt',
-   division_id => 'parent-problem',
-   expected =>
-   {
-    valid_semantics_warning => 'INVALID EXPLICIT DECLARATION OF INFER-ONLY PROPERTY',
-   },
-  };
+    print "document is valid\n";
+  }
 
-  # arrange
-  my $tcname   = $tc->{name};
-  my $testfile = $tc->{testfile};
-  my $id       = $tc->{division_id};
-  my $fragment = $parser->create_fragment($testfile);
-  my $division = $library->get_division($id);
-  my $expected = $tc->{expected}{valid_semantics_warning};
-# my $t1logger = Test::Log4perl->get_logger('sml.Division');
-
-#  Test::Log4perl->start( ignore_priority => "info" );
-#  $t1logger->warn(qr/$expected/);
-
-  # act
-my $result = $division->has_valid_semantics;
-
-  # assert
-#  Test::Log4perl->end("$tcname $expected");
+else
+  {
+    print "DOCUMENT IS NOT VALID\n";
+  }
 
 ######################################################################
 
