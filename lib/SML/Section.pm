c7af6f36b932f27b3e2c91343d3e32fbe5d95e91
Don Johnson <drj826@acm.org>
2015-12-26 17:17:05 -0700

    Added new SML::Section public method 'encloses_division_with_name' That checks a section and all its subsections for a division with the specified name.  This allowed me to suppress top level headings for empty front matter listings (issue #25)

--- lib/SML/Section.pm
+++ lib/SML/Section.pm
@@ -74,6 +74,57 @@ has top_number =>
 ######################################################################
 ######################################################################
 
+sub encloses_division_with_name {
+
+  # Return 1 if this section 'encloses' a division with the specified
+  # name.  If the section in question is chapter 3, this means return
+  # 1 if any sub-section of chapter 3 contains a division with the
+  # specified name.
+  #
+  # Sections exist in sequences and cannot be nested.  This may seem
+  # counterintuitive.  A section may not contain another section?
+  # What about sub-sections?  Technically, sections don't *contain*
+  # sub-sections.  Sections have properties like depth and number that
+  # make them appear nested, when in fact they are not.
+  #
+  # If you want to know if chapter 3 (or any of its sub-sections)
+  # contains a table, you must check each sub-section individually.
+  # That's what this method does.
+  #
+  # This method takes advantage of the way sections are numbered:
+  #
+  #   3
+  #   3.1
+  #   3.2
+  #   3.2.1
+  #   3.2.2
+  #
+  # It assumes one section 'encloses' another if the section number of
+  # the first matches the beginning of the second.  For instance, this
+  # method assumes section 3.2 encloses section 3.2.2.
+
+  my $self = shift;
+  my $name = shift;
+
+  my $number       = $self->get_number;
+  my $document     = $self->get_containing_document;
+  my $section_list = $document->get_list_of_divisions_with_name('SECTION');
+
+  foreach my $section (@{ $section_list })
+    {
+      my $section_number = $section->get_number;
+
+      next unless $section_number =~ /^$number/;
+
+      if ( $section->contains_division_with_name($name) )
+	{
+	  return 1;
+	}
+    }
+
+  return 0;
+}
+
 ######################################################################
 
 no Moose;
