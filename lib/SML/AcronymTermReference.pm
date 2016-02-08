19ba17e20d61e197e2eed06816dfadd72414fbc8
Don Johnson <drj826@acm.org>
2016-02-07 20:26:21 -0700

    Updated POD documentation in AcronymTermReference.pm and Block.pm

--- lib/SML/AcronymTermReference.pm
+++ lib/SML/AcronymTermReference.pm
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 
-package SML::AcronymTermReference;
+package SML::AcronymTermReference;      # ci-000440
 
 use Moose;
 
@@ -13,6 +13,56 @@ use namespace::autoclean;
 use Log::Log4perl qw(:easy);
 with 'MooseX::Log::Log4perl';
 my $logger = Log::Log4perl::get_logger('sml.AcronymTermReference');
+
+######################################################################
+
+=head1 NAME
+
+SML::AcronymTermReference - a reference to an acronym term
+
+=head1 SYNOPSIS
+
+  SML::AcronymTermReference->new
+    (
+      tag     => $tag,                  # Str
+      acronym => $acronym,              # SML::Definition
+    );
+
+  $ref->get_tag;                        # Str
+  $ref->get_namespace;                  # Str
+  $ref->get_acronym;                    # SML::Definition
+
+=head1 DESCRIPTION
+
+SML::AcronymTermReference extends L<SML::String> to represent a
+reference to an acronym term.
+
+The acronym reference tag has the following significance:
+
+=over 4
+
+=item
+
+a => If this is the first occurence render the long version otherwise
+render the short version.
+
+=item
+
+ac => Same as 'a'
+
+=item
+
+acs => Render the short version of the acronym.
+
+=item
+
+acl => Render the long version of the acronym.
+
+=back
+
+=head1 METHODS
+
+=cut
 
 ######################################################################
 ######################################################################
@@ -30,6 +80,15 @@ has tag =>
    required => 1,
   );
 
+=head2 get_tag
+
+Return a scalar text value which is the tag portion of the acronym
+term reference.
+
+  my $tag = $acronym_term_reference->get_tag;
+
+=cut
+
 ######################################################################
 
 has acronym =>
@@ -40,6 +99,14 @@ has acronym =>
    required => 1,
   );
 
+=head2 get_acronym
+
+Return the acronym definition (an L<SML::Definition>).
+
+  my $acronym = $acronym_term_reference->get_acronym;
+
+=cut
+
 ######################################################################
 
 has namespace =>
@@ -49,6 +116,14 @@ has namespace =>
    reader   => 'get_namespace',
    default  => '',
   );
+
+=head2 get_namespace
+
+Return the scalar text value for the namespace.
+
+  my $namespace = $acronym_term_reference->get_namespace;
+
+=cut
 
 ######################################################################
 
@@ -102,47 +177,13 @@ __PACKAGE__->meta->make_immutable;
 
 __END__
 
-=head1 NAME
-
-C<SML::AcronymTermReference> - a reference to an acronym term
-
-=head1 VERSION
-
-2.0.0
-
-=head1 SYNOPSIS
-
-  extends SML::String
-
-  my $ref = SML::AcronymTermReference->new
-              (
-                tag     => $tag,
-                acronym => $acronym,
-              );
-
-  my $string  = $ref->get_tag;
-  my $acronym = $ref->get_acronym;
-  my $string  = $ref->get_namespace;
-
-=head1 DESCRIPTION
-
-Extends L<"SML::String">.  Represents a reference to an acronym term.
-
-=head1 METHODS
-
-=head2 get_tag
-
-=head2 get_acronym
-
-=head2 get_namespace
-
 =head1 AUTHOR
 
 Don Johnson (drj826@acm.org)
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright (c) 2012,2013 Don Johnson (drj826@acm.org)
+Copyright (c) 2012,2016 Don Johnson (drj826@acm.org)
 
 Distributed under the terms of the Gnu General Public License (version
 2, 1991)
